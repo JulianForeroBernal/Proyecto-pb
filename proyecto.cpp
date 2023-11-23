5ed3241b6bbb0e10cf8097d8691466aee1c965d1
Julian Forero Bernal <julianforerobernal@gmail.com>
2023-11-23 12:41:31 -0500

    mm

--- proyecto.cpp
+++ proyecto.cpp
@@ -3,11 +3,16 @@
 using namespace std;
 
 int main(){
-    int arbusto=0;
+    int arbusto=0, count=5;
     system("cls");
     cout<<"hola";
     cin>>arbusto;
     cout<<arbusto;
+    for (int i = 0; i < count; i++)
+    {
+        cout<<1<<endl;
+    }
+    
 
 
 }
